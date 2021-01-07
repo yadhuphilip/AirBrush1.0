9af93e30900bd68f54eeea53ef004894e08b93f9
Yadhu Philip <yadhuphilip@gmail.com>
2021-01-08 02:53:15 +0530

    Added ClearScreen, Almost Zero noise, NEED TO AVOID SMALL JERKS OR KEEP THE CENTER STABLE

--- AirBrush.py
+++ AirBrush.py
@@ -2,9 +2,19 @@ import cv2
 import numpy as np
 
 
+###################
+
+def clearScreenCheck(xy):
+
+    if xy[0]>=0 and xy[0]<59 and xy[1] >0 and xy[1] <74: return True
+    return False
+
+##################
+
+
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+# cap.set(3,1280)
+# cap.set(4,720)
 trail = []
 while True:
 
@@ -13,7 +23,9 @@ while True:
     frame = cv2.flip(frame, 1)
 
 
-    blured = cv2.blur(frame, (10,10))
+    blured = cv2.GaussianBlur(frame, (7,7),0)
+    #blured = cv2.medianBlur(frame, 5)
+    #blured = cv2.blur(frame, (10,10))
     hsv = cv2.cvtColor(blured,  cv2.COLOR_BGR2HSV)
 
     ## Masking only light-Green
@@ -27,16 +39,22 @@ while True:
     #print(len(contours))
     #cv2.drawContours(frame, contours, -1, (0,255,0), 2)
     ##Strengthen the selection
-    if len(contours) >  0  and cv2.contourArea(max(contours, key = cv2.contourArea)) > 1000:
+    cv2.rectangle(frame, (0,0),(60,75), (0,0,0), -1)
+    if len(contours) >  0  and cv2.contourArea(max(contours, key = cv2.contourArea))>150:
         largest_contour = max(contours, key = cv2.contourArea)
         x,y,w,h = cv2.boundingRect(largest_contour)
         center = x +w//2 , y+ h//2
         trail.append((center))
+        if clearScreenCheck(center):
+            trail = []
         #cv2.rectangle(frame,(x,y),(x+w, y+h), (0,255,0), -1)
         cv2.circle(frame, (center), 8, (0,255,0), -1)
+    if len(trail)>2:
+        for i in range(1,len(trail[:-1])):
+            x1,y1 = trail[i-1]
+            x2,y2 = trail[i]
+            if abs(x1-x2) >1 or abs(y2-y1)>1: cv2.line(frame, trail[i-1], trail[i], (0,0,255), 2)
 
-    for i in range(1,len(trail[:-1])):
-        cv2.line(frame, trail[i-1], trail[i], (0,0,255), 1)
 
     cv2.imshow("frame", frame)
 
